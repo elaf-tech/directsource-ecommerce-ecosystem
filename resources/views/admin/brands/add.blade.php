63e7b56096470d7c7a057fbba463a039a05644b6
Elaf Alhadaie <elaffalhadaie@gmail.com>
2026-05-03 13:52:41 +0300

    Security: Remove exposed OpenAI API key

--- resources/views/admin/brands/add.blade.php
+++ resources/views/admin/brands/add.blade.php
@@ -233,4 +233,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-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
+{{-- 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 --}}
